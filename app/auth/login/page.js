3bba790277787a7523674ec3d735ee271295016f
jardeljasse <123008389+jardeljasse@users.noreply.github.com>
2024-05-01 16:13:42 +0200

    aa

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -6,7 +6,7 @@ export default function page(){
     return(
         <main className="h-[100vh] pt-[10vh]">
             <section className="bg-green-100 max-w-sm mx-auto  p-2 flex items-center flex-col">
-                <Image src="/images/gre.jpg"  width={250} height={250} alt="Green Capital Logistic Image"/>
+                <Image src="/images/sen-logo.png"  width={250} height={250} alt="Sen logo"/>
 
                 <LoginForm />
             </section>
