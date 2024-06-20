1bc347fc5ff32a7b2df30080e17e6d22630cb4e7
jardeljasse <123008389+jardeljasse@users.noreply.github.com>
2024-06-20 17:42:34 +0200

    answer schema

--- models/doubtsTabLessonModel.js
+++ models/doubtsTabLessonModel.js
@@ -1,10 +1,9 @@
 import {model, models, Schema} from "mongoose";
 
+const answerLessonSchema = new Schema({
 
-const doubtsLessonSchema = new Schema({
-    doubt: {
+    answer:{
         type: String,
-        required: true,
     },
     user:{
         type: Schema.Types.ObjectId,
@@ -20,6 +19,28 @@ const doubtsLessonSchema = new Schema({
         required: true,
     }
 });
+
+const doubtsLessonSchema = new Schema({
+    doubt: {
+        type: String,
+        required: true,
+    },
+    answer:answerLessonSchema,
+    user:{
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: "User", 
+    },
+    createdAt:{
+        type: Date,
+        required: true,
+    },
+    updatedAt: {
+        type: Date,
+        required: true,
+    }
+});
+
 
 const DoubtsLesson_Model = models.DoubtsLesson_Model || model("DoubtsLesson_Model", doubtsLessonSchema);
 export default DoubtsLesson_Model;
