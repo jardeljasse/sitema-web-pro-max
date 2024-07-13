52574c30e610f661f7324030c04e0ae2c4a6da75
jardeljasse <123008389+jardeljasse@users.noreply.github.com>
2024-07-13 10:17:05 +0200

    to apresente

--- components/LessonFormVercel.js
+++ components/LessonFormVercel.js
@@ -1,31 +1,40 @@
 'use client';
- 
+
 import { upload } from '@vercel/blob/client';
 import { useState, useRef } from 'react';
- 
+
 export default function LessonFormVercel() {
+
+  const [isLoading, setIsLoading] = useState(false)
   const inputFileRef = useRef(null);
   const [blob, setBlob] = useState(null);
   return (
     <>
-      <h1>Upload Your Avatar</h1>
- 
+      {/* <h1>Upload Your Avatar</h1> */}
+
       <form
         onSubmit={async (event) => {
           event.preventDefault();
- 
+
           const file = inputFileRef.current.files[0];
- 
+
           const newBlob = await upload(file.name, file, {
             access: 'public',
             handleUploadUrl: '/api/upload',
           });
- 
+
           setBlob(newBlob);
+          setIsLoading(true)
         }}
       >
-        <input name="file" ref={inputFileRef} type="file" required />
-        <button className='p-1 bg-cyan-700 text-white' type="submit">Upload</button>
+        <p>
+          <label htmlFor="file" className='block'>Selecionar aula</label>
+          <input name="file" id='file' ref={inputFileRef} className='max-w-md' type="file" required />
+          </p>
+        <button
+          disabled={isLoading}
+          className='
+        disabled:bg-skin-cl700p-1 w-full bg-cyan-700 text-white' type="submit">Upload</button>
       </form>
       {blob && (
         <div>
