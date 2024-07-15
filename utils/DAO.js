9acf4936d9b6294a7221f2ee6e1e26591b3191de
jardeljasse <123008389+jardeljasse@users.noreply.github.com>
2024-07-15 15:29:37 +0200

    DAO-actived

--- utils/DAO.js
+++ utils/DAO.js
@@ -4,6 +4,6 @@ import { connect } from "mongoose"
 
 export const connectToDB = async () => {
 
-    await connect ("mongodb://localhost:27017/plataforma-crei")
-    // await connect ("mongodb+srv://jardeljasse:jardeljasse18@cluster0.otcsboc.mongodb.net/plataforma-crei")
+    // await connect ("mongodb://localhost:27017/plataforma-crei")
+    await connect ("mongodb+srv://jardeljasse:jardeljasse18@cluster0.otcsboc.mongodb.net/plataforma-crei")
 }
